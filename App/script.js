73661f53b2b06213e4263a6556e74319760ff7f6
Piyush Kriplani <piyushkriplani444@gmail.com>
2022-11-13 18:42:33 +0530

    Added selection sort in script with button disable and enable  function and added sound

---
+++ App/script.js
@@ -0,0 +1,98 @@
+let bars = [];
+const def = "#fd0081", chng = "#431f91", finished = "#8ef511", selected = "yellow";
+
+window.onload = setup();
+async function setup() {
+	let b = document.getElementById("bars");
+	let d = document.getElementById("delay");
+	document.getElementById("b").innerText = b.value;
+	document.getElementById("d").innerText = d.value + "ms";
+
+	if (bars.length != parseInt(b.value)) {
+		generateBars(parseInt(b.value));
+	}
+}
+
+
+function reset() {
+	location.reload();
+}
+
+
+function Disable_The_Input() {
+	let x = document.getElementsByTagName("input");
+	for (let i = 0; i < x.length; i++)
+		x[i].disabled = true;
+	return parseInt(document.getElementById("delay").value);
+}
+
+
+function Finished_Sorting() {
+	let x = document.getElementsByClassName("bar");
+	for (let i = 0; i < x.length; i++)
+		x[i].style.backgroundColor = finished;
+	x = document.getElementsByTagName("input");
+	for (let i = 0; i < x.length; i++)
+		x[i].disabled = false;
+
+}
+
+
+function generateBars(n = -1) {
+	bars = [];
+	let container = document.getElementById("container");
+	n = n < 0 ? Math.random() * 20 : n;
+	for (let i = 0; i < n; i++) {
+		bars.push('<div class="bar" id="' + i + '" style="height:' + Math.floor(2 + Math.random() * 98) + '%"></div>');
+	}
+	container.innerHTML = bars.join('');
+
+}
+
+
+function Sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function MapRange(value, in_min, in_max, out_min, out_max) {
+	return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+async function SelectionSort() {
+	let delay = Disable_The_Input();
+
+	let container = document.getElementById("container");
+	for (let i = 0; i < bars.length; i++) {
+		let mn_ind = i;
+		let curr_id = bars[i].split('id="')[1].split('"')[0];
+		document.getElementById(curr_id).style.backgroundColor = selected;
+		let sound = MapRange(document.getElementById(curr_id).style.height.split('%')[0], 2, 100, 500, 1000);
+		beep(100, sound, delay)
+		for (let j = i + 1; j < bars.length; j++) {
+			let nxt_ele = bars[j].split('id="')[1].split('"')[0];
+			document.getElementById(nxt_ele).style.backgroundColor = chng;
+			let a = parseInt(bars[mn_ind].split(/[:%]/)[1]);
+			let b = parseInt(bars[j].split(/[:%]/)[1]);
+			if (a > b) mn_ind = j;
+			await Sleep(delay / 5.0);
+			document.getElementById(nxt_ele).style.backgroundColor = def;
+		}
+
+		let nxt_ele = bars[mn_ind].split('id="')[1].split('"')[0];
+		document.getElementById(nxt_ele).style.backgroundColor = selected;
+		await Sleep(2 * delay / 5.0);
+
+		let tmp = bars[mn_ind];
+		bars[mn_ind] = bars[i];
+		bars[i] = tmp;
+
+		container.innerHTML = bars.join('');
+		await Sleep(2 * delay / 5.0);
+		document.getElementById(curr_id).style.backgroundColor = def;
+		document.getElementById(nxt_ele).style.backgroundColor = def;
+	}
+	Finished_Sorting();
+}
+
+
+
